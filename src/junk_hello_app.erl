6523c04cf9b37632c1caebb5cb9719e6f9aaca69
Benjamin Jenson <benjamin.jenson11@yahoo.com>
2024-03-06 10:12:24 -0700

    second commit

--- src/junk_hello_app.erl
+++ src/junk_hello_app.erl
@@ -10,7 +10,7 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    io:format("Hello world!",[]),
+    io:format("Hello world!~nIt worked!~n",[]),
     junk_hello_sup:start_link().
 
 stop(_State) ->
