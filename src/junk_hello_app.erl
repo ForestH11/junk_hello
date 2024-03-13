60d2c4e0c2de7439bcbaa1315cd04f6be609da39
Benjamin Jenson <benjamin.jenson11@yahoo.com>
2024-03-13 09:28:51 -0600

    added tls support

--- src/junk_hello_app.erl
+++ src/junk_hello_app.erl
@@ -10,7 +10,21 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    io:format("Hello world!~nIt worked!~n",[]),
+    Dispatch = cowboy_router:compile([
+	    {'_', [
+		%{"/", cowboy_static, {priv_file, db_access, "static/index.html"}},
+	        {"/", default_page_h, []}
+
+	    ]}
+	]),
+
+	PrivDir = code:priv_dir(junk_hello),
+	%tls stands for transport layer security
+        {ok,_} = cowboy:start_tls(https_listener, [
+                  		{port, 443},
+				{certfile, PrivDir ++ "/ssl/fullchain.pem"},
+				{keyfile, PrivDir ++ "/ssl/privkey.pem"}
+              		], #{env => #{dispatch => Dispatch}}),
     junk_hello_sup:start_link().
 
 stop(_State) ->
